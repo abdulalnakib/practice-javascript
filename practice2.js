b2ad377de57b602f3a309f7f5735d39aec882783
abdulalnakib <abdulalnakib10@gmail.com>
2024-03-08 20:33:47 +0600

    finished the second  practice

---
+++ practice2.js
@@ -0,0 +1,11 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalMarks = mathematics + biology + chemistry + physics + bangla;
+var subjects = 5;
+var average = totalMarks / subjects;
+average = average.toFixed(2);
+average = parseFloat(average);
+console.log(average)
